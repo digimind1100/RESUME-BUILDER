67a69315edd168b9c55a32672a025eba00a097e2
haider-zaidi72 <suhz1100@gmail.com>
2025-11-04 10:14:20 +0500

    addUpdate4-11-25

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
   const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLinkClick = () => setMenuOpen(false); // close menu on link click
 
   return (
     <nav className="navbar">
@@ -14,31 +17,64 @@ export default function Navbar() {
         </Link>
       </div>
 
-      {/* Nav Links */}
-      <ul className="navbar-links">
+      {/* Hamburger */}
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+
+      {/* Links */}
+      <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
         <li>
-          <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+          <Link
+            to="/"
+            className={location.pathname === "/" ? "active" : ""}
+            onClick={handleLinkClick}
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link to="/features" className={location.pathname === "/features" ? "active" : ""}>
+          <Link
+            to="/features"
+            className={location.pathname === "/features" ? "active" : ""}
+            onClick={handleLinkClick}
+          >
             Features
           </Link>
         </li>
         <li>
-          <Link to="/pricing" className={location.pathname === "/pricing" ? "active" : ""}>
+          <Link
+            to="/pricing"
+            className={location.pathname === "/pricing" ? "active" : ""}
+            onClick={handleLinkClick}
+          >
             Pricing
           </Link>
         </li>
         <li>
-          <Link to="/faq" className={location.pathname === "/faq" ? "active" : ""}>
+          <Link
+            to="/faq"
+            className={location.pathname === "/faq" ? "active" : ""}
+            onClick={handleLinkClick}
+          >
             FAQ
+          </Link>
+        </li>
+
+        {/* Mobile Resume Button */}
+        <li className="mobile-resume-btn">
+          <Link to="/resume" className="resume-btn" onClick={handleLinkClick}>
+            Start Builder
           </Link>
         </li>
       </ul>
 
-      {/* Resume Builder Button */}
+      {/* Desktop Resume Button */}
       <div className="navbar-btn">
         <Link to="/resume" className="resume-btn">
           Start Builder
