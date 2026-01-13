b4a7417c89e9d42d537574c44b117648ce2b1549
haider-zaidi72 <suhz1100@gmail.com>
2026-01-13 20:09:29 +0500

    Fix staging password

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { authGate } from "./authGate";
-
-authGate();
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { authGate } from "./authGate"; // 🔒 staging lock
+
+// 🔐 Run gate BEFORE React renders
+authGate();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
