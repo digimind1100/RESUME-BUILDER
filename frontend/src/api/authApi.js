d541759d6162247564265cf76c4dbc01b4436ed2
haider-zaidi72 <digimind1100@gmail.com>
2026-01-16 22:56:53 +0500

    Add import.meta.env.VITE_API_BASE_URL

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
 ﻿/* src/api/authApi.js */
-
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+  import.meta.env.VITE_API_URL || "http://localhost:3001";
+
 
 async function handleJsonResponse(response) {
   let data;
