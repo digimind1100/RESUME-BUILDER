983cb8c78cf1fa911b348980ca7fff421bca01ad
urooj haider <suhz1100@gmail.com>
2026-01-24 14:13:06 +0500

    test 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>HOME OK</h1>} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>HOME OK</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
