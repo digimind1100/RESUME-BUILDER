8ad0d4ac82aedefb233b24a9d461a9bc0972d787
haider-zaidi72 <digimind1100@gmail.com>
2026-02-05 17:15:06 +0500

    update App.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ import CounterBlock from "./CounterBlock";
 import Footer from "./Footer";
 import TemplateShowcase from "./TemplateShowcase";
 import AiTutorialVideo from "./AiTutorialVideo"
-import ReviewsSection from "../components/ReviewsSection";
+
 
 
 
