761811b29fb663711315e3cd19e5a8548c6f11fd
urooj haider <suhz1100@gmail.com>
2026-02-10 13:10:00 +0500

    Fix ReviewContext.jsx (send userId)

--- frontend/src/context/ReviewContext.jsx
+++ frontend/src/context/ReviewContext.jsx
@@ -33,7 +33,7 @@ const submitReview = async (data) => {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-  userId: user.id, 
+  userId: user._id || user.id, // 🔥 THIS FIXES DUPLICATE KEY ERROR
   name: data.name,
   rating: data.rating,
   comment: data.review,
