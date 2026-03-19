cadb1f5079e925dad6937943701ae6280bb817a9
haider-zaidi72 <digimind1100@gmail.com>
2026-03-19 18:47:41 +0500

    update

--- frontend/src/Templates/MinimalAccent.jsx
+++ frontend/src/Templates/MinimalAccent.jsx
@@ -1,6 +1,7 @@
 // src/Templates/MinimalAccent.jsx
 import React, { useRef, useState } from "react";
 import "./MinimalAccent.css";
+import TemplateLayout from "./TemplateLayout";
 import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../context/AuthContext";
