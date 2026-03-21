453717b48cbd745251b22fe46924f8d5baf7d0a3
haider-zaidi72 <digimind1100@gmail.com>
2026-03-22 01:00:51 +0500

    update

--- frontend/src/Template/TemplateLayout.jsx
+++ frontend/src/Template/TemplateLayout.jsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from "react";
-import templates from "./index"
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import TemplateControls from "./TemplateControls";
