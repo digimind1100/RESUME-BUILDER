641579acc2f1104648db2caa7f6fee29dcd05db6
haider-zaidi72 <digimind1100@gmail.com>
2026-03-21 16:41:54 +0500

    remove PDF

--- frontend/src/Templates/AviationPro/AviationPro.jsx
+++ frontend/src/Templates/AviationPro/AviationPro.jsx
@@ -450,7 +450,7 @@ Profile: ${profileLink}
           </div>
 
           {/* A4 PAGE */}
-          <div className="resume-a4 av-a4" ref={pdfRef} style={{ position: "relative" }}>
+          <div className="resume-a4 av-a4" style={{ position: "relative" }}>
 
             <div className="av-resume" contentEditable={false}>
 
