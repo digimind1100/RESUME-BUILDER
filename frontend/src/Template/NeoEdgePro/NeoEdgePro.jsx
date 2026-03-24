bbeb82a9626417fecf85b8aab6d86725759d4f06
urooj haider <suhz1100@gmail.com>
2026-03-24 15:02:24 +0500

    update css

--- frontend/src/Template/NeoEdgePro/NeoEdgePro.jsx
+++ frontend/src/Template/NeoEdgePro/NeoEdgePro.jsx
@@ -14,7 +14,7 @@ export default function NeoEdgePro() {
       {({ canEdit, isEditable, pdfRef, requirePayment }) => (
 
         <div className="neo-wrapper">
-          resume-a4 neo-a4
+          <div className="resume-a4 neo-a4" ref={pdfRef}>
             <div className="resume-a4 neo-a4">
 
               <div className="neo-resume">
