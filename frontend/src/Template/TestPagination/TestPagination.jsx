2937828bc01889724525bb876adc8d329d7a8c98
haider-zaidi72 <digimind1100@gmail.com>
2026-03-27 20:17:28 +0500

    update widht

--- frontend/src/Template/TestPagination/TestPagination.jsx
+++ frontend/src/Template/TestPagination/TestPagination.jsx
@@ -190,7 +190,7 @@ export default function TestPagination() {
         style={{
           position: "absolute",
           visibility: "hidden",
-          width: "972px",
+          width: "210mm",
         }}
       >
         {entries.map((entry) => (
