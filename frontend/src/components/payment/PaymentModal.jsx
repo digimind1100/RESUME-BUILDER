ed06028a18e67fbd162079fdfd3e02c72cc53b2f
urooj haider <suhz1100@gmail.com>
2026-06-10 13:01:25 +0500

    update qr code image

--- frontend/src/components/payment/PaymentModal.jsx
+++ frontend/src/components/payment/PaymentModal.jsx
@@ -5,7 +5,7 @@ import "./PaymentModal.css";
 import easypaisaLogo from "../../assets/payments/easypaisa.png";
 import jazzcashLogo from "../../assets/payments/jazzcash.png";
 import sadapayLogo from "../../assets/payments/sadapay.png";
-import easypaisaQR from "../../assets/payments/easypaisa-qr.png";
+import easypaisaQR from "../../assets/payments/easypaisa-qr.jpeg";
 
 import { useAuth } from "../../context/AuthContext";
 
